75759a8cfe4502558b1b199e7fa85a9a2f661c24
zahra nathania <146054158+natanzra@users.noreply.github.com>
2024-03-19 10:36:58 +0700

    Update data_balok.php

--- pertemuan 4/tugas1/data_balok.php
+++ pertemuan 4/tugas1/data_balok.php
@@ -1,15 +1,4 @@
 <?php 
-		/**
-		 * Task 2
-		 * Panggil class_balok dan Buatlah minimal 4 object yang menampilkan:
-		 * Luas, Keliling dan Volume
-		 * p = 29, l = 16, t = 7
-		 */
-
-		// code..
-?>
-
-<?php 
     require_once 'class_balok.php';
     
     $balok1 = new Balok(29, 16, 7);
